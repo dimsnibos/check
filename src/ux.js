0c707c2db49b889077725df45d5960513a09cb76
dimsnibos <you@example.com>
2025-09-01 10:27:37 +0700

    anjay mabar ganti lagi

--- src/ux.js
+++ src/ux.js
@@ -1,32 +1,42 @@
+const silang = document.getElementById("silang")
+const link = document.getElementById("linkwoi")
+link.style.display = "none"
+silang.addEventListener("click",()=>{
+    if (link.style.display === "none"){
+        link.style.display = "block"
+    }
+    else{
+        link.style.display = "none"
+    }
+})
+
 const load = document.getElementById("loading")
 const body = document.body
 function instagram(){
     body.style.opacity = "0.7"
     load.style.display = "flex"
-
     setTimeout(()=>{
-        load.style.display = "none"
-        window.location.href="https://instagram.com/ptramadian_"
-          body.style.transition = "all 0.3s ease-in-out"
-    },2000)
-    
+    body.style.opacity = "1"
+    load.style.display = "none"
+    window.location.href = "https://instagram.com/ptramadian_"
+    },1200)
 }
 
 const value = document.getElementById("value")
 const calcLeft = document.getElementById("calc-left")
 const calcRight = document.getElementById("calc-right")
-const numbers = ["1",'2','3','4','5','6','7','8','9','(','0',')']
-const operations = ["+","-","/","x"]
 
-
+const numbers = ["1","2","3","4","5","6","7","8","9","(","0",")"]
 numbers.forEach((number)=>{
     const button = document.createElement("button")
     button.innerHTML = number
+    calcLeft.append(button)
     button.addEventListener("click",()=>{
         value.innerHTML += number
     })
-    calcLeft.append(button)
 })
+
+const operations = ["+","-","x","/"]
 operations.forEach((operation)=>{
     const button = document.createElement("button")
     button.innerHTML = operation
@@ -37,15 +47,14 @@ operations.forEach((operation)=>{
 })
 
 const del = document.createElement("button")
-del.innerHTML="DEL"
-calcRight.append(del)
+del.innerHTML = "DEL"
 del.addEventListener("click",()=>{
     value.innerHTML = value.innerHTML.slice(0, -1)
 })
+calcRight.append(del)
 
-const enter= document.createElement("button")
-enter.innerHTML="ENTER"
-calcRight.append(enter)
+const enter = document.createElement("button")
+enter.innerHTML = "ENTER"
 enter.addEventListener("click",()=>{
     let ekspresi = value.innerHTML
     ekspresi = ekspresi.replace(/x/g, "*")
@@ -54,39 +63,35 @@ enter.addEventListener("click",()=>{
     }
     catch(error){
         value.innerHTML = error
-        setTimeout(()=>{
-            value.innerHTML = ""
-        },1200)
     }
 })
+calcRight.append(enter)
 
 const clear = document.createElement("button")
-clear.innerHTML="CLEAR"
-calcRight.append(clear)
+clear.innerHTML = "CLEAR"
 clear.addEventListener("click",()=>{
-    value.innerHTML = "CLEAR THE AREA"
+    value.innerHTML = "CLEAR THE AREA...."
     setTimeout(()=>{
-         value.innerHTML = ""
+        value.innerHTML = " "
     },1200)
 })
+calcRight.append(clear)
 
-init = 0
-const warna = ["red","blue","white","brown",'transparent']
-const bgrond = document.createElement("button")
-bgrond.innerHTML = "BGR"
-calcRight.append(bgrond)
-bgrond.addEventListener("click",()=>{
-    init ++
-    const page2 = document.getElementById("parallax2")
-    page2.style.backgroundColor = warna[init]
-
+const changeBg = document.createElement("button")
+changeBg.innerHTML = "BGR"
+const parallax2 = document.getElementById("parallax2")
+let enit = 0
+const warna = ["red","white","yellow","brown","transparent"]
+changeBg.addEventListener("click",()=>{
+    parallax2.style.backgroundColor = warna[enit]
 })
+calcRight.append(changeBg)
 
 const piano = document.getElementById("chord")
 const chords = ["a","c","d","e","f"]
 chords.forEach((chord)=>{
     const button = document.createElement("button")
-    button.innerHTML = chord
+    button.innerText = chord
     piano.append(button)
     button.addEventListener("click",()=>{
         const sound = new Audio(`asset/${chord}.wav`)
@@ -98,129 +103,50 @@ const user = document.getElementById("userTanya")
 const botAnswer = document.getElementById("botJawab")
 
 function botSay(data){
-    return [
-        `Hello, i'm dimskuyBot, What's Yourname??`,
-        `Ouhh ${data?.nama}, where are you from?`,
-        `Ohh i see youre from ${data?.kota}, what's your goals?`,
-        `Wow ${data?.goals}, such a good goals!`,
+    return[
+        `Hello, Im DimsKuy Who are you??`,
+        `Hi ${data?.nama}, where are you from?`,
+        `Ohh youre from ${data?.kota}, Whats your hobby? `,
+        `Wow ${data?.hobi}, such a good hobby..`,
     ]
 }
 
-let enit = 0
-const userData = [ ]
+let userData = [ ]
 botAnswer.innerHTML = botSay()[0]
+let init = 0
 
 function submit(){
-    enit = enit + 1
-    if (enit === 1){
+    init++
+    if(init === 1){
         responseBot({nama : user.value})
-        user.value = " "
     }
-    else if (enit === 2){
+    else if (init === 2){
         responseBot({kota : user.value})
-        user.value = " "
     }
-    else if (enit === 3){
-        responseBot({goals : user.value})
-        user.value = " "
+    else if (init === 3){
+        responseBot({hobi : user.value})
+    }
+    else if (init === 4){
         user.style.display = "none"
-    }
-    else if (enit === 4){
-         load.style.display = "flex"
-    body.style.opacity = "0.7"
+        botAnswer.innerHTML = "Restarting bottt...."
         setTimeout(()=>{
-window.location.reload()
-        },1200)
-        
-       
+              load.style.display = "flex"
+              body.style.opacity = "0.7"
+        },300)
+        setTimeout(()=>{
+            window.location.reload()
+        },2000)
     }
-
 }
 
 function responseBot(jawabanUser){
     userData.push(user.value)
-    load.style.display = "flex"
-    body.style.opacity = "0.7"
-    setTimeout(()=>{
-         load.style.display = "none"
-    body.style.opacity = "1"
-    botAnswer.innerHTML = botSay(jawabanUser)[enit]
-    },1500)
-    
+      load.style.display = "flex"
+      body.style.opacity = "0.7"
+      setTimeout(()=>{
+        load.style.display = "none"
+        body.style.opacity = "1"
+        botAnswer.innerText = botSay(jawabanUser)[init]
+      },1200)
+
 }
-
-const silang = document.getElementById("silang")
-const link = document.getElementById("linkwoi")
-link.style.display = "none"
-
-silang.addEventListener("click",()=>{
-   
-    if (link.style.display === "none"){
-        link.style.display = "block"
-    }
-    else{
-        link.style.display = "none"
-    }   
-})
-// function botSay(data){
-//     return [
-//      'Hello! my name is DimsKuyBot, Who`s your name?',           //Fungsi ini bikin list pertanyaan (pakai array).           
-//      `Hii ${data?.nama}, How old are youu?`,                     // 'data?' itu optional chaining. Jadi kalau datanya belum ada, gak error
-//      `Ouhh ${data?.usia}, what about your goals?`,               //- Gunanya buat personalisasi pertanyaan sesuai jawaban user sebelumnya 🫶
-//      `woww ${data?.citaCita}, such a good goal, Goodluck!`,
-// ]
-// }
-// botAnswer.innerText = botSay()[0]
-// enit = 0
-// let Data = [ ]
-
-// function submit(){
-
-//     enit = enit + 1
-//     if(enit === 1){
-//         dataUser({nama : user.value}) 
-//             user.value = ""
-//     }
-//     else if(enit === 2){
-//         dataUser({usia : user.value})
-//         setTimeout(()=>{
-//             user.value = ""
-//         },2000)
-//     }
-//      else if(enit === 3){
-//         dataUser({citaCita : user.value})
-//         user.value = "thanks"
-//     }
-//      else if(enit === 4){
-//        window.location.reload ()
-//     }
-// }
-//  const silang = document.getElementById("silang")
-//  const linkwoi = document.getElementById("linkwoi")
-// function link() {
-//     linkwoi.style.display = "block";
-
-//     silang.addEventListener("click", () => {
-    
-//         // Toggle display
-//         if (linkwoi.style.display === "block") {
-//             linkwoi.style.display = "none";
-//         } else {
-//             linkwoi.style.display = "block";
-
-//         }
-//     });
-// }
-// function dataUser(jawabanUser){
-//     Data.push(user.value)
-//     body.style.opacity = "0.7"
-//     load.style.display = "flex"
-
-//     setTimeout(()=>{
-//             botAnswer.innerHTML = botSay(jawabanUser)[enit]
-//             body.style.opacity = "1"
-//     load.style.display = "none"
-//     body.style.transition = "all 0.3s ease-in-out"
-//         },2000)
-
-// }
