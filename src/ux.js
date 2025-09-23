6f451d2757730bcb29943606ba92f810149afb4c
dimsnibos <you@example.com>
2025-09-23 12:54:25 +0700

    done?

--- src/ux.js
+++ src/ux.js
@@ -1,12 +1,13 @@
-let namalu = prompt("masukkan nama anda")
+let namalu = prompt("masukkan nama lu")
 if(namalu != "Dimas"){
-    alert("HELLOWWW WELCOMEE "+ namalu)
+    alert("HIII WELCOMEE " + namalu)
     const load = document.getElementById("loading")
 const body = document.body
 
 const silang = document.getElementById("silang")
 const link = document.getElementById("linkwoi")
 link.style.display = "none"
+
 silang.addEventListener("click",()=>{
     if(link.style.display === "none"){
         link.style.display = "block"
@@ -20,12 +21,12 @@ function instagram(){
     body.style.opacity = "0.7"
     setTimeout(()=>{
         window.location.href = "https://instagram.com/ptramadian_"
-    },1200)
+    },1200) 
 }
 
+const value = document.getElementById("value")
 const calcLeft = document.getElementById("calc-left")
 const calcRight = document.getElementById("calc-right")
-const value = document.getElementById("value")
 
 const numbers = ["1","2","3","4","5","6","7","8","9","(","0",")"]
 numbers.forEach((number)=>{
@@ -49,24 +50,13 @@ operations.forEach((operation)=>{
 
 const del = document.createElement("button")
 del.innerHTML = "DEL"
+calcRight.append(del)
 del.addEventListener("click",()=>{
     value.innerHTML = value.innerHTML.slice(0, -1)
 })
-calcRight.appendChild(del)
-
-const clear = document.createElement("button")
-clear.innerHTML = "CLEAR"
-clear.addEventListener("click",()=>{
-    value.innerHTML = "CLEAR THE AREA..."
-    setTimeout(()=>{
-        value.innerHTML = " "
-    },1200)
-})
-calcRight.append(clear)
 
 const enter = document.createElement("button")
-enter.innerHTML = "enter"
-calcRight.append(enter)
+enter.innerHTML = "ENTER"
 enter.addEventListener("click",()=>{
     let ekspresi = value.innerHTML
     ekspresi = ekspresi.replace(/x/g,"*")
@@ -80,29 +70,40 @@ enter.addEventListener("click",()=>{
         },1200)
     }
 })
+calcRight.append(enter)
 
-const warna = ["red","white","blue","brown","white","transparent"]
-let enit = 0
+const clear = document.createElement("button")
+clear.innerHTML = "CLEAR"
+calcRight.append(clear)
+clear.addEventListener("click",()=>{
+    value.innerHTML = "CLEAR THE AREA"
+    setTimeout(()=>{
+        value.innerHTML = " "
+    },1200)
+})
+
+const warna = ["red","white","blue","brown","transparent"]
 const parallax2 = document.getElementById("parallax2")
+let initt = 0
 
 const bgr = document.createElement("button")
 bgr.innerHTML = "BGR"
 calcRight.append(bgr)
 bgr.addEventListener("click",()=>{
-    enit = enit + 1
-    parallax2.style.backgroundColor = warna[enit]
+    initt = initt + 1
+    parallax2.style.backgroundColor = warna[initt]
 })
 
 const piano = document.getElementById("chord")
-const chords = ["a","c","d","e","f"]
+const chords = [ "a","c","d","e","f"]
 chords.forEach((chord)=>{
     const button = document.createElement("button")
     button.innerHTML = chord
-    piano.append(button)
     button.addEventListener("click",()=>{
         const sound = new Audio(`asset/${chord}.wav`)
         sound.play()
     })
+    piano.append(button)
 })
 
 const user = document.getElementById("userTanya")
@@ -110,38 +111,34 @@ const botAnswer = document.getElementById("botJawab")
 
 function botSay(data){
     return[
-    `Hoy, Whats your name?`,
-        `Hi ${data?.nama}, where are you from?`,
-        `Ouh youre from ${data?.kota}, whats your favourite game?`,
-        `Wow ${data?.game} thats interesting!`,
-]
+        `Whats your namee?`,
+        `Hii ${data?.nama}, where are you from?`,
+        `Ouhh youre from ${data?.kota}, whats your favourite game?`,
+        `Wow ${data?.game}, thats interesting!`,
+    ]
 }
 
-let init = 0
 let userData = [ ]
+let init = 0
 botAnswer.innerHTML = botSay()[0]
 
 function submit(){
-    init++
+    init = init + 1
     if(init === 1){
-        responseBot({nama : user.value})
+    responseBot({nama : user.value})
     }
-    else if(init === 2){
-        responseBot({kota : user.value})
+    else if (init === 2){
+    responseBot({kota : user.value})
     }
-    else if(init === 3){
+    else if (init === 3){
         responseBot({game : user.value})
         user.style.display = "none"
     }
-    else if(init === 4){
-        botAnswer.innerHTML = `Nice to know ya!`
-        setTimeout(()=>{
-            load.style.display = "flex"
-            body.style.opacity = "0.7"
-        },1000)
+    else if (init === 4){
+        botAnswer.innerHTML = "Nice to know ya!"
         setTimeout(()=>{
             window.location.reload()
-        },3000)
+        },1200)
     }
 }
 
@@ -152,13 +149,13 @@ function responseBot(jawabanUser){
     setTimeout(()=>{
         load.style.display = "none"
         body.style.opacity = "1"
-        botAnswer.innerHTML = botSay(jawabanUser)[init]
         user.value = ""
+        botAnswer.innerHTML = botSay(jawabanUser)[init]
     },1200)
 }
 }
 else{
-    alert ("Nama lu "+namalu+", SALAH")
+    alert("ASUU")
     setTimeout(()=>{
         window.location.reload()
     },1200)
